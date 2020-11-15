4273a098b2aaf43947d9a4538910c4a60209516b
prabhat1420 <58967330+prabhat1420@users.noreply.github.com>
2020-11-15 22:25:44 +0545

    tours and travels

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tours and Travels</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+		<!-- Google Fonts -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+		<!-- Bootstrap core CSS -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Material Design Bootstrap -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+				
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<a class="navbar-brand" href="#">Tours and Travels</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="about.php">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Services</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="border-radius: 5px">Search</button>
+				</form>
+			</div>
+		</nav>
+		<div id="demo" class="carousel slide" data-ride="carousel">
+			<ul class="carousel-indicators">
+				<li data-target="#demo" data-slide-to="0" class="active"></li>
+				<li data-target="#demo" data-slide-to="1"></li>
+				<li data-target="#demo" data-slide-to="2"></li>
+			</ul>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="images/3.jpg" class="img-fluid" alt="..." width=100% height=100%>
+					<div class="carousel-caption">
+						<h3>Welcome To Nepal</h3>
+						<p>Home of Paradise</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/2.jpg" class="img-fluid" alt="..." width=100%>
+					<div class="carousel-caption">
+						<h3>Welcome To Nepal</h3>
+						<p>Home of Paradise</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/1.jpg" class="img-fluid" alt="..." width=100%>
+					<div class="carousel-caption">
+						<h3>Welcome To Nepal</h3>
+						<p>Home of Paradise</p>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#demo" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+			</a>
+			<a class="carousel-control-next" href="#demo" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+			</a>
+		</div>
+		<section class="my-5">
+			<div class="py-4">
+				<h2 class="text-center">About Us</h2>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					
+					<div class="col-md-6 col-lg-6 col-xl-6 col-12">
+						<img src="images/11.jpg" class="img-fluid about-img">
+						
+					</div>
+					<div class="col-md-6 col-lg-6 col-xl-6 col-12">
+						<h3 class="p-2">Tours and Travels</h3>
+						<p class="display-5 p-2">We are one of the leading online travel agency in Nepal.We provide leisure holidays and adventure activities in Nepal.Our mission is to provide our clients unforgettable travel experiences through our wide range of high quality yet good value packages. We assure you of our most competitive prices with utmost attention and best services at all times.</p>
+						<a href="about.php" class="btn btn-outline-success" style="border-radius: 5px">Know More</a>
+						
+					</div>
+				</div>
+				
+			</div>
+		</section>
+		<section>
+			<div class="py-4">
+				<h2 class="text-center">Our Services</h2>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xl-4 col-lg-4 col-md-4 col-12">
+						<div class="card">
+							<img class="card-img-top img-fluid img-info" src="images/trek.jpg" alt="Card image">
+							<div class="card-body">
+								<h4 class="card-title">Trekking</h4>
+								<p class="card-text">Trek in the mountains</p>
+								<a href="#" class="btn btn-primary stretched-link" style="border-radius: 5px">Know more</a>
+							</div>
+						</div>
+						
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-4 col-12">
+						<div class="card">
+							<img class="card-img-top img-fluid img-info" src="images/horse.jpg" alt="Card image">
+							<div class="card-body">
+								<h4 class="card-title">Adrenaline Rush</h4>
+								<p class="card-text">Horse riding, Rock climbing, And many more...</p>
+								<a href="#" class="btn btn-primary stretched-link" style="border-radius: 5px">Know more</a>
+							</div>
+						</div>
+						
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-4 col-12">
+						<div class="card">
+							<img class="card-img-top img-fluid img-info" src="images/culture.jpg" alt="Card image">
+							<div class="card-body">
+								<h4 class="card-title">Cultural Visit</h4>
+								<p class="card-text">Know the diversity in culture and enjoy the traditions.</p>
+								<a href="#" class="btn btn-primary stretched-link" style="border-radius: 5px">Know more</a>
+							</div>
+						</div>
+						
+					</div>
+					
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="py-4">
+				<h2 class="text-center">LOGIN</h2>
+			</div>
+			<form action="users_info.php" style="width: 400px; margin: auto;">
+				<div class="form-group">
+					<label for="email">Email address:</label>
+					<input type="email" class="form-control" placeholder="Enter email" id="email">
+				</div>
+				<div class="form-group">
+					<label for="pwd">Password:</label>
+					<input type="password" class="form-control" placeholder="Enter password" id="pwd">
+				</div>
+				<div class="form-group form-check">
+					<label class="form-check-label">
+						<input class="form-check-input" type="checkbox"> Remember me
+					</label>
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</section>
+		
+		<footer class="page-footer bg-dark mt-5">
+			<div class="bg-success" style="height: 50px;">
+				<div class="container">
+					<div class="row py-3 d-flex align-items-center">
+						<div class="col-md-12 text-center">
+							<a href="#"><i class="fab fa-facebook-f test-white mr-4"></i></a>
+							<a href="#"><i class="fab fa-twitter test-white mr-4"></i></a>
+							<a href="#"><i class="fab fa-google-plus test-white mr-4"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></i></a>
+						</div>
+						
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="container-fluid text-center text-md-left mt-5 ml-5">
+				<div class="row">
+					<div class="col-md-3 mx-auto mb-4">
+						<h5 class="text-uppercase font-weight-bold">CONTACT INFO</h5>
+						<hr class="bg-success mb-4 mt-0 d-inline-block mx-auto " style="width: 140px; height: 2px">
+						<ul class="list-unstyled">
+							<li class="my-2">Tours and Travels</li>
+							<li class="my-2">Suite 303 level 3</li>
+							<li class="my-2">468 George Street</li>
+							<li class="my-2">Sydney NSW 2000</li>
+							<li class="my-2">info@toursandtravels.com.au</li>
+							<li class="my-2">0280948024</li>
+						</ul>
+					</div>
+					<div class="col-md-3 mx-auto mb-4">
+						<h5 class="text-uppercase font-weight-bold">INFORMATION</h5>
+						<hr class="bg-success mb-4 mt-0 d-inline-block mx-auto " style="width: 140px; height: 2px">
+						<ul>
+							<li class="my-2"><a href="#">Trekking Equipment Checklist</a></li>
+							<li class="my-2"><a href="#">Money Exchange Rates</a></li>
+							<li class="my-2"><a href="#">Travel Guide in Nepal</a></li>
+							<li class="my-2"><a href="#">Sitemap</a></li>
+							<li class="my-2"><a href="#">Blog</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3 mx-auto mb-4">
+						<ul class="list-unstyled">
+							<li class="my-3"><a href="#">Contact Us</a></li>
+							<li class="my-3"><a href="#">About Us</a></li>
+							<li class="my-3"><a href="#">Privacy Policy</a></li>
+							<li class="my-3"><a href="#">Terms Of Use</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+			<div class="footer-copyright text-center py-2" style="height: 40px;">
+				<p>&copy;2020 Tours and Travels Pvt. Ltd. All rights reserved</p>
+			</div>
+			
+		</footer>
+
+
+
+
+
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+				<!-- JQuery -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<!-- Bootstrap tooltips -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+		<!-- Bootstrap core JavaScript -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+		<!-- MDB core JavaScript -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+	</body>
+</html>
